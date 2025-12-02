8e38aeee87a77677be1ce398cd529d89d7258c54
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-12-02 21:14:45 +0100

    Create Halfspinonextremevaluesheliumtableerrorslookuponfirst.h

---
+++ Halfspinonextremevaluesheliumtableerrorslookuponfirst.h
@@ -0,0 +1,8 @@
+//the bottleneck here would be to overcome asymptotic
+//values that differs from real numbers on a dirzv equation
+//form suited for heat transfer. we dont need mass transfer then here this deal with several models.
+//the oarc model must be fullfilled with black boxes if datas are extreme isotropic values
+//then this will be a topology algorithm mainly. the remaining part would be to not post process the traces of black boxes models.
+//so we do it twicd this is what we want to prevent errors lookup.
+//plus, the dpin would be kept in another topologiczl equation. so the forces acting on oarc and black boxes models are not safe.
+//but havin bad valies for spins algebras is not a bad thing if we think it seriously (another topoc).
